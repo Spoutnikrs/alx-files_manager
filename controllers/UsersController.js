597afe7a1e40567e9fd2ca64891df716ab94c799
rayesyounes <rayesyounes88@gmail.com>
2024-12-08 12:41:49 +0100

    init api

---
+++ controllers/UsersController.js
@@ -0,0 +1,36 @@
+import sha1 from "sha1";
+import dbClient from "../utils/db";
+
+
+class UsersController {
+    static postNew(request, response) {
+        const {email} = request.body;
+        const {password} = request.body;
+
+        if (!email) {
+            response.status(400).json({error: "Missing email"});
+            return;
+        }
+        if (!password) {
+            response.status(400).json({error: "Missing password"});
+            return;
+        }
+
+        const users = dbClient.db.collection("users");
+        users.findOne({email}, (err, user) => {
+            if (user) {
+                response.status(400).json({error: "Already exist"});
+            } else {
+                const hashedPassword = sha1(password);
+                users.insertOne({
+                    email, password: hashedPassword,
+                },).then((result) => {
+                    response.status(201).json({id: result.insertedId, email});
+                }).catch((error) => console.log(error));
+            }
+        });
+    }
+
+}
+
+module.exports = UsersController;
